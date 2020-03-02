bc8658ed98c5cf50dee5d96d15ff7c3f4032cdfd
Roberto Gil <robgilmx@gmail.com>
2020-03-02 10:07:37 -0600

    Small search clear fix

--- src/app/pages/deals-search/deals-search.page.ts
+++ src/app/pages/deals-search/deals-search.page.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { LoadingController } from '@ionic/angular';
 import { CheapsharkProvider } from 'src/app/providers/cheapshark';
-import { STORES_DATA } from 'src/app/constants/stores'
+import { STORES_DATA } from 'src/app/constants/stores';
 
 @Component({
   selector: 'app-deals-search',
@@ -9,49 +9,51 @@ import { STORES_DATA } from 'src/app/constants/stores'
   styleUrls: ['deals-search.page.scss']
 })
 export class DealsSearchPage {
-  private searchTerm: string = '';
-  private numberPage:number = 0;
-  private games = []
-  private loader: any;
-  private lastPageReached = false
-  private scrollContent: any;
-  private deepScroll = false;
 
   constructor(
     private loadingController: LoadingController,
-    private cheapshark: CheapsharkProvider){}
+    private cheapshark: CheapsharkProvider) {}
+  private searchTerm = '';
+  private numberPage = 0;
+  private games = [];
+  private loader: any;
+  private lastPageReached = false;
+  private scrollContent: any;
+  private deepScroll = false;
+
+  private scrollDepthTriggered = false;
 
     onSearch() {
     this.resetList();
-    if(this.searchTerm == ""){
+    if (this.searchTerm === '') {
       this.games = [];
-    }else{
-      this.showLoader()
+    } else {
+      this.showLoader();
       this.getGames();
       this.hideLoader();
     }
   }
 
-    resetList(){
+    resetList() {
      this.numberPage = 0;
-     this.games = []; 
+     this.games = [];
     }
 
   getGames(): void {
-    this.cheapshark.getStoreDeals(this.searchTerm,this.numberPage)
+    this.cheapshark.getStoreDeals(this.searchTerm, this.numberPage)
     .subscribe((response) => {
-      this.lastPageReached = response == [];
-      this.games.push.apply(this.games,response);
+      this.lastPageReached = response === [];
+      this.games.push.apply(this.games, response);
       this.numberPage += 1;
       console.log(response);
-    },(error) => { 
-      alert('No data available'); 
+    }, (error) => {
+      alert('No data available');
     });
   }
 
   loadData(event) {
     setTimeout(() => {
-      this.getGames()
+      this.getGames();
       event.target.complete();
       event.target.disabled = this.lastPageReached;
     }, 500);
@@ -62,46 +64,48 @@ export class DealsSearchPage {
       message: 'Looking for deals'
     }).then((res) => {
       res.present();
- 
+
       res.onDidDismiss().then((dis) => {
       });
     });
   }
- 
+
   hideLoader() {
     setTimeout(() => {
       this.loadingController.dismiss();
     }, 500);
   }
 
-  getScrollPosition(){
+  getScrollPosition() {
 
   }
 
-  scrollToTop(){
-    if(this.scrollContent == null){
+  scrollToTop() {
+    if (this.scrollContent == null) {
       this.scrollContent = document.getElementById('listScroll');
     }
-    this.scrollContent.scrollToTop(1000);
+    this.scrollContent.scrollToTop();
   }
-
-  private scrollDepthTriggered = false;
   async logScrolling($event) {
-    if(this.scrollDepthTriggered) {
+    if (this.scrollDepthTriggered) {
       return;
     }
 
-    if($event.target.localName != "ion-content") {
+    if ($event.target.localName !== 'ion-content') {
       return;
     }
 
     const currentScrollDepth = $event.detail.scrollTop;
-    this.deepScroll = currentScrollDepth > 200
-    //console.log({currentScrollDepth});
+    this.deepScroll = currentScrollDepth > 200;
+    // console.log({currentScrollDepth});
   }
 
-  getStoreName(id){
+  getStoreName(id) {
     return STORES_DATA.find(i => i.storeID === id).storeName;
+  }
+
+  onClear() {
+      this.games = [];
   }
 
 }
